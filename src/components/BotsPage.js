e341d783406209ad1186ed510a298a78fba1dc08
Jaffar Hussein <jaffhussein@icloud.com>
2022-10-02 16:04:10 +0200

    Deliverable-1 :See profiles of all bots rendered in BotCollection

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
   //start here with your code for step one
+  const URL = "http://localhost:8002/bots"
+  const [bots, setBots] = useState([]);
+  const [myBots,setMyBots] = useState([]);
 
+
+  useEffect(() => {
+    fetch(URL)
+    .then(r => r.json())
+    .then(data => setBots(data))
+  },[])
+  const updateCollection = (id) => {
+    // console.log(bots.filter(bot => bot.id === id));
+    setMyBots(
+      [...myBots,bots.filter(bot => bot.id === id)[0]]
+    )
+    // console.log(myBots);
+  }
   return (
     <div>
-      <YourBotArmy />
-      <BotCollection />
+      <YourBotArmy myBots={myBots}/>
+      <BotCollection bots={bots} updateCollection={updateCollection}/>
     </div>
   )
 }
